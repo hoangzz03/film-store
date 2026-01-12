f3926ce136645d9746df02751977ad0c1a291266
hoang <hoangnv1415@gmail.com>
2026-01-12 11:30:45 +0700

    change cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8888'],
+    origin: ['http://103.75.182.180/, http://103.75.182.180/8080'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
